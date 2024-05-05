13989ac30e88cee3ab442460f8e40795ed27aaba
Mohammed Alobaidi <contact@mohalobaidi.com>
2024-05-05 23:18:17 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  devServer: { port: 8081 },
-  modules: ["@nuxt/ui"]
+  devServer: { port: 8081 }
 })
